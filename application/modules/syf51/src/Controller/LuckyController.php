0964fff608153ef53dae25070b51fb25c3c533d2
Emmanuel ROY <ornithorynxe@live.fr>
2019-11-23 03:00:32 +0100

    Ajout d'une autre application symfony 5.0.99 afin de tester le partage de la variable $_SESSION -->OK
    TODO: ajouter un plug-in symfony permettant de charger un utilisateur dans les apps a partir de l'authentification multiple
    
    TODO: lire les documentation officielles provenant des 4 plate-formes tranquillement afin de comprendre commet doit on tester ces type d'auth quitte a créé un sous domaine particulier directement hebergé sur gittea
     -->Sécuriser le serveur de dev

--- application/modules/syf51/src/Controller/LuckyController.php
+++ application/modules/syf51/src/Controller/LuckyController.php
@@ -1,12 +1,15 @@
 <?php
+// src/Controller/LuckyController.php
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-class DefaultController extends Controller
+use Symfony\Component\HttpFoundation\Request;
+
+class LuckyController extends AbstractController
 {
     /**
-     * @Route("/syf43", name="homepage")
+     * @Route("/syf51", name="homepage")
      */
     public function indexAction(Request $request)
     {
@@ -14,7 +17,7 @@ class DefaultController extends Controller
         print_r($this->get('session'));
         print_r($_COOKIE);
         print_r($_SESSION);
-        $_SESSION['test-user43'] = "user43";
+        $_SESSION['test-user51'] = "user51";
         // replace this example code with whatever you need
         return $this->render('default/page.html.twig', [
             'text' => 'homepage',
@@ -22,7 +25,7 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/syf43/page1", name="page1")
+     * @Route("/syf51/page1", name="page1")
      */
     public function page1Action(Request $request)
     {
@@ -31,14 +34,33 @@ class DefaultController extends Controller
             'text' => 'page1',
         ]);
     }
+
     /**
-     * @Route("/syf43/page2", name="page2")
+     * @Route("/syf51/page2", name="page2")
      */
     public function page2Action(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/page.html.twig', [
             'text' => 'page2',
+        ]);
+    }
+
+    /**
+     * @Route("/syf51/number")
+     */
+    public function number()
+    {
+
+        print_r("<pre>");
+        print_r($this->get('session'));
+        print_r($_COOKIE);
+        print_r($_SESSION);
+
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
         ]);
     }
 }
