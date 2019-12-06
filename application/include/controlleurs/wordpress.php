be5f63ebd9fbcda7e93291980994d15d2c02e40c
Emmanuel ROY <ornithorynxe@live.fr>
2019-12-06 17:35:12 +0100

    Ajout et tests des modules Wordpress et Prestashop Installation finalisée pour wordpress Installation en erreur pour Prestashop

---
+++ application/include/controlleurs/wordpress.php
@@ -0,0 +1,29 @@
+<?php
+\MVC\Classe\Session::start();
+$app = new MVC\Classe\Modular($name,'wordpress',$url_params);
+//echo "Dawn Wordpress Error!";
+/*
+ * Avoid Warning on my version ....
+ *
+  on application/modules/wordpress/wp-admin/menu-header.php:74
+	if(!is_array($menu)){$menu = array();}
+
+  on application/modules/wordpress/wp-admin/includes/plugin.php:2047
+
+        if(isset($_wp_menu_nopriv)) {
+            foreach (array_keys($_wp_submenu_nopriv) as $key) {
+                if (isset($_wp_submenu_nopriv[$key][$pagenow])) {
+                    return false;
+                }
+                if (isset($plugin_page) && isset($_wp_submenu_nopriv[$key][$plugin_page])) {
+                    return false;
+                }
+            }
+        }
+
+
+ */
+
+
+echo $app->load('wordpress'); die();
+$templateData = array('app' => $app);
