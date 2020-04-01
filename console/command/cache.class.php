3c457d3af81958fa4c7daa79bcbd8f0329021377
Emmanuel ROY <ornithorynxe@live.fr>
2020-04-01 09:45:51 +0200

    Test sur la création des modules par une commande console -> OK! Test sur une api rest -> OK! Test sur le deplacement d'une autre page de compte -> OK!
    TODO: créé les pages de blog (sommaire,news) permettant d'afficher les fichiers md

--- console/command/cache.class.php
+++ console/command/cache.class.php
@@ -9,10 +9,17 @@ class cache
     }
 
     static public function clear(){
-        print "cache cleared ! \n\n";
+        $git_cache_rm = system('rm -f '.VIEW_PATH.'/cache/*', $git_cache_rm_retval);
+        print $git_cache_rm_retval;
+        $git_logs_rm = system('rm -f '.LOG_PATH.'/*', $git_logs_rm_retval);
+        print $git_logs_rm_retval;
+
+        print "logs && cache cleared ! \n\n";
     }
 
     static public function stabilize(){
+        $git_cache_rm = system('rm -f '.VIEW_PATH.'/cache/*', $git_cache_rm_retval);
+        print $git_cache_rm_retval;
         print "cache stabilized ! \n\n";
     }
 
