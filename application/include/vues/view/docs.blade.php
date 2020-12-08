d29e57ba2ba60bcad175d12830a9fd47cfc9f349
Emmanuel ROY <ornithorynxe@live.fr>
2020-12-08 08:47:44 +0100

    repositionnement du bin.php pour les commandes ajout de pages sur le template polo (docs,about,cgu) modification du fichier de bdd

--- application/include/vues/view/docs.blade.php
+++ application/include/vues/view/docs.blade.php
@@ -6,6 +6,9 @@
 
 @section('content')
     @if (isset($files))
+
+        <h1>Sommaire:</h1>
+
         @foreach( $files as $file)
             <a href="{{ \MVC\Classe\Url::link_rewrite( false, 'docs', ['file'=>$file]) }}">{{ $file }}</a> <br/>
         @endforeach
