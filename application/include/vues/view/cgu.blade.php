b3de5ea6625d6351af8227b70bf6178fccb9d49b
Emmanuel ROY <ornithorynxe@live.fr>
2020-12-18 20:20:37 +0100

    update dev files

--- application/include/vues/view/cgu.blade.php
+++ application/include/vues/view/cgu.blade.php
@@ -7,10 +7,9 @@
 @section('content')
     <div class="container">
         <h1>Conditions Générale de l'application:</h1>
-        <br/>
-        <br/>
+
         <p>1. Cette application est en phase de test et n'est qu'un test.</p><br/>
         <p>2. Si cela est autre chose se référer au point 1.</p><br/>
-        <p>3. Cette application peut permettre le développement du framework (SAND) dont la documentation sera écrite en MarkDown et publié par un moteur interne au framework.</p><br/>
+        <p>3. Cette application permet le developpement d'un frame-work propre dont la documentation sera écrite en MarkDown et publié par un moteur interne à l'application</p><br/>
     </div>
 @endsection
