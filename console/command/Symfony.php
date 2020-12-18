d08d258877c7650d0070076773370920ff8fd5d7
Emmanuel ROY <ornithorynxe@live.fr>
2020-12-18 22:30:48 +0100

    update gitignore

--- console/command/Symfony.php
+++ console/command/Symfony.php
@@ -12,6 +12,6 @@ class Symfony
     public static function stabilize()
     {
         print "stabilize symfony module...\n\n";
-        $symfony_module = shell_exec('sudo cp '.CONSOLE_PATH.'/skel/symfony '.VENDOR_PATH.' -Rf');
+        //$symfony_module = shell_exec('sudo cp '.CONSOLE_PATH.'/skel/symfony '.VENDOR_PATH.' -Rf');
     }
 }
