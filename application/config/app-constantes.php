2bf8665d14749c895dd982d0d105b1cc3dfd1fbf
Emmanuel ROY <ornithorynxe@live.fr>
2020-12-18 19:32:30 +0100

    update HEAD diff

--- application/config/app-constantes.php
+++ application/config/app-constantes.php
@@ -14,12 +14,3 @@ define("TRAITEMENT_PATH", APPLICATION_PATH . DIRECTORY_SEPARATOR . "traitements"
 define("PUBLIC_PATH", dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . "public");
 define("CONSOLE_PATH", dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . "console");
 define("DATA_PATH", dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . "data");
-
-<<<<<<< HEAD:application/config/define-constantes.php
-// De base laisser vide,
-// sauf si l'application est hebergé sur de multiples sous-repertoire en ajoutant le slash_final
-// par exemple : "my-app/"
-//          ou : "my-app/dev/
-define("BASE_SERVER_DIRECTORY", "");
-=======
->>>>>>> master-dev:application/config/app-constantes.php
--- application/include/vues/system/system.blade.php
+++ application/include/vues/system/system.blade.php
@@ -9,14 +9,10 @@
     <meta name="description" lang="fr" content="{{$description}}"/>
 
     @section('top-css')
-<<<<<<< HEAD
         <link rel="stylesheet" href="{{ \MVC\Classe\Url::asset_rewrite('assets/css/main.css')}}">
         <link rel="stylesheet" href="{{ \MVC\Classe\Url::asset_rewrite('assets/css/plugins.css')}}">
         <link rel="stylesheet" href="{{ \MVC\Classe\Url::asset_rewrite('assets/css/style.css')}}">
-=======
-        <link rel="stylesheet" href="{{ \MVC\Classe\Url::asset_rewrite('assets/bootstrap-5.0.0-beta1-dist/css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{ \MVC\Classe\Url::asset_rewrite('assets/css/custom.css')}}">
->>>>>>> master-dev
     @show
 
 </head>
@@ -29,16 +25,12 @@
 @yield('body')
 
 @section('bottom-javascript')
-<<<<<<< HEAD
     <script src="{{ \MVC\Classe\Url::asset_rewrite('assets/js/script.js')}}" defer="defer"></script>
     <script src="{{ \MVC\Classe\Url::asset_rewrite('assets/js/jquery.js')}}"></script>
     <script src="{{ \MVC\Classe\Url::asset_rewrite('assets/js/plugins.js')}}"></script>
     <!--Template functions-->
     <script src="{{ \MVC\Classe\Url::asset_rewrite('assets/js/functions.js')}}"></script>
-=======
-    <script src="{{ \MVC\Classe\Url::asset_rewrite('assets/bootstrap-5.0.0-beta1-dist/js/bootstrap.min.js')}}"></script>
     <script src="{{ \MVC\Classe\Url::asset_rewrite('assets/js/custom.js')}}"></script>
->>>>>>> master-dev
 @show
 
 </body>
