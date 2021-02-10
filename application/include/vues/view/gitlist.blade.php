ffce2a8ca9e02762ab68c411ed9d6fe9a5cb7538
Emmanuel ROY <emmanuel.roy@univ-fcomte.fr>
2021-02-10 09:05:08 +0100

    update: gitlist intégration

--- application/include/vues/view/gitlist.blade.php
+++ application/include/vues/view/gitlist.blade.php
@@ -1,1 +1,11 @@
-{{$app->load('gitlist')}}
+@extends('body')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    {{$app->load('gitlist')}}
+@endsection
