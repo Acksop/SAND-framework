7a3cf99bea8c142ed9a8fb110d4567fd4a2d5251
Emmanuel ROY <emmanuel.roy@univ-fcomte.fr>
2021-06-18 17:16:09 +0200

    Adding build structure for SAND FrameWork on version 3.8.0

--- build/build.sh
+++ build/build.sh
@@ -7,13 +7,17 @@
 php ./bin/phpDocumentor.phar -d ../application -d ../console -d ../domain --ignore "vendor/*,build/*,data/*,tests/*,console/skel/*,application/modules/*,application/inculde/vues/cache/*" -t ./data/api-docs/
 
 # PHP MESS DETECTOR
-php ./bin/phpmd.phar ../ html codesize,design,naming,unusedcode --exclude '*vendor*' --exclude '*tests*' --exclude '*build*' --exclude '*data*' --exclude '*skel*' --exclude '*modules*' --exclude '*cache*' > ./data/phpmd.html
+php ./bin/phpmd.phar ../ html codesize,naming,unusedcode --exclude '*vendor*' --exclude '*tests*' --exclude '*build*' --exclude '*data*' --exclude '*skel*' --exclude '*modules*' --exclude '*cache*' ./data/phpmd.html
+php ./bin/phpmd.phar ../ xml codesize,naming,unusedcode --exclude '*vendor*' --exclude '*tests*' --exclude '*build*' --exclude '*data*' --exclude '*skel*' --exclude '*modules*' --exclude '*cache*' ./logs/phpmd.xml
+
+#CHECKSTYLE OF THE PROJECT
+./bin/phpcs -p --extensions=php --ignore=*/vendor/*,*/build/*,*/data/*,*/test/*,*/skel/*,*/modules/*,*/cache/*,*/public/* --report-xml=./logs/checkstyle.xml ../
 
 # CHARTS OF PROJECTS
 ./bin/pdepend --jdepend-chart=data/jdepend-chart.svg --overview-pyramid=data/jdepend-overview.svg --summary-xml=data/jdepend-summary.xml --ignore=vendor,tests,build,data,console/skel,application/modules,application/inculde/vues/cache ../
 
 # TEXT METRIC OF PROJECT
-php ./bin/phploc-7.0.2.phar ../ --exclude ../vendor --exclude ../build --exclude ../tests --exclude ../data --exclude ../console/skel --exclude ../application/modules --exclude ../application/include/vues/cache > data/phploc.txt
+php ./bin/phploc-7.0.2.phar ../ --exclude ../vendor --exclude ../build --exclude ../tests --exclude ../data --exclude ../console/skel --exclude ../application/modules --exclude ../application/include/vues/cache --log-xml logs/phploc.xml > data/phploc.txt
 
 # DUPLICATED LINES OF PROJECT
 php ./bin/phpcpd-6.0.3.phar --exclude ../vendor --exclude ../build --exclude ../tests --exclude ../data --exclude ../console/skel --exclude ../application/modules --exclude ../application/include/vues/cache ../ > data/phpcpd.txt
